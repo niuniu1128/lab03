7dc593686449b1b1611d31d4bb64613002db8040
niuniu1128 <161405952+niuniu1128@users.noreply.github.com>
2024-03-30 14:06:55 +0800

    Update lab307.java

---
+++ lab307.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class lab307 {
+    public class JPA03 {
+    public static void main (String argv[]) throws IOException {
+    	BufferedReader br =
+    			new BufferedReader(new InputStreamReader(System.in));
+        int num1, num2;
+        
+        System.out.println("Input:");
+        num1 = Integer.parseInt(br.readLine());
+        
+        while (num1 != 999) {
+            num2 = Integer.parseInt(br.readLine());
+            System.out.println(gcd(num1, num2));
+            System.out.println("Input:");
+            num1 = Integer.parseInt(br.readLine());
+        }
+    }
+
+    static int gcd (int m, int n) {
+        int result = m, temp;
+        while (n != 0){
+            if(m % n == 0) {
+            	result = n;
+            	break;
+            }
+            else {
+            	temp = n;
+            	n = m % n;
+            	m = temp;
+            }
+        }
+        return result;
+    }
+}
+}
